55f9c7238981a591b2537f6354271ca59e377872
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-08-08 09:04:25 +0300

    Update

--- my_hostel/__manifest__.py
+++ my_hostel/__manifest__.py
@@ -1,8 +1,11 @@
 {
     "name": "Hostel Management",  # Module title
     "summary": "Manage Hostel easily",  # Module subtitle phrase
-    "description": "Efficiently manage the entire residential facility in the school.",
-    # Supports reStructuredText(RST) format (description is Deprecated)
+    "description": """
+Manage Hostel
+==============
+Efficiently manage the entire residential facility in the school
+    """,  # Supports reStructuredText(RST) format (description is Deprecated)
     "version": "17.0.1.0.0",
     "author": "Serpent Consulting Services Pvt. Ltd.",
     "category": "Tools",
@@ -12,7 +15,11 @@
     "data": [
         "security/hostel_security.xml",
         "security/ir.model.access.csv",
+        "data/data.xml",
         "views/hostel.xml",
+        "views/hostel_room.xml",
+        "views/hostel_amenities.xml",
+        "views/hostel_student.xml",
     ],
     # This demo data files will be loaded if db initialize with demo data (commented because file is not added in this example)
     # 'demo': [
