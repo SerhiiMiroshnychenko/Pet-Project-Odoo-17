45119d30edc893bb23c4de35db7dfe4aa2791acf
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-08-14 08:58:10 +0300

    Update

--- my_hostel/models/hostel_room_copy.py
+++
@@ -1,7 +0,0 @@
-from odoo import fields, models, api, _
-
-
-class HostelRoomCopy(models.Model):
-    _name = "hostel.room.copy"
-    _inherit = "hostel.room"
-    _description = "Hostel Room Information Copy"
