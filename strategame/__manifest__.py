b4a0d50a85c8595bc9d61634fa01caa01e2f0851
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-08-27 09:34:04 +0300

    Update

---
+++ strategame/__manifest__.py
@@ -0,0 +1,38 @@
+################################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2024 Serhii Miroshnychenko (https://github.com/SerhiiMiroshnychenko).
+#
+################################################################################
+
+{
+    "name": "Strategame",
+    "summary": "Strategic Game in Odoo",
+    "description": """
+Strategame
+======================
+Strategic Game in Odoo
+""",
+    "version": "17.0.1.0.0",
+    "author": "Serhii Miroshnychenko",
+    "website": "https://github.com/SerhiiMiroshnychenko",
+    "license": "OPL-1",
+    "category": "Extra Tools",
+    "depends": [],
+    "data": [
+        "security/ir.model.access.csv",
+
+        "views/game_views.xml",
+        "views/homeland_views.xml",
+    ],
+    "assets": {
+        "web.assets_frontend": [],
+        "web.assets_backend": [
+            '/mir_imp/static/src/css/*.css',
+        ],
+    },
+    "external_dependencies": {},
+    "installable": True,
+    "auto_install": False,
+    "application": True,
+}
