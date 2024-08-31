bc7b65ac9a1eafd33473af1fb806a0def101b930
Serhii Miroshnychenko <veprzlisu@gmail.com>
2024-08-31 09:19:28 +0300

    Update

--- strategame/model/homeland.py
+++ strategame/model/homeland.py
@@ -6,6 +6,10 @@ class HomeLand(models.Model):
     _description = 'Country in the Game'
 
     name = fields.Char(string='Homeland', required=True)
-    gold = fields.Monetary()
+    gold = fields.Monetary(default=100.0)
     currency_id = fields.Many2one('res.currency', default=lambda self: self.env['res.currency'].search([('name', '=', 'USD')]))
-    
+    game_id = fields.Many2one('strategic.game')
+    farms = fields.Integer(string='Farms', default=1)
+    army = fields.Integer(string='Army', default=50)
+
+
